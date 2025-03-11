52d85d68ee55de9408d9a6437662c4cbf4199e0d
Raphael <raphael@zeropath.com>
2025-03-11 11:42:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ auth_service = AuthService()
 payroll_service = PayrollService()
 
 
+
 def get_user(username):
     query = "SELECT * FROM users WHERE username = '" + username + "'"
     return db.execute(query) 

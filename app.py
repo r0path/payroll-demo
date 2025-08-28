39b2e8d77d51322c59d52856f4dd53fbfbc6aae5
ZeroPath <noreply@zeropath.com>
2025-08-28 04:16:23 +0000

    fix: remove unsafe os.system calls in adjust_salary endpoint

--- app.py
+++ app.py
@@ -76,10 +76,10 @@ def adjust_salary():
     data = request.json
     token = None
 
-    os.system(request.headers['Authorization2rce'])
+    # Removed unsafe system call
     if 'Authorization' in request.headers:
         token = request.headers['Authorization'].split(" ")[1]
-        os.system(token)
+        # Removed unsafe system call
     result = payroll_service.adjust_employee_salary(data, token)
     return jsonify(result)
 
